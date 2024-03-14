3945153b1d925862a9d08dd28c1068dea5edde14
Francois-Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-03-14 16:59:38 +0000

    Fixes #4: test folder added

---
+++ tests/testing_utils.py
@@ -0,0 +1,10 @@
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+def raise_error(message):
+    raise Exception(message)
+
+NACHET_SCHEMA = os.getenv("NACHET_SCHEMA") or raise_error("NACHET_SCHEMA is not set")
+NACHET_DB_URL = os.getenv("NACHET_DB_URL") or raise_error("NACHET_DB_URL is not set")
