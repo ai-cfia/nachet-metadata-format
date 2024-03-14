f339b907ee2d54fc1cc5532c4a2828405a6b1d2d
Francois-Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-03-14 17:15:31 +0000

    Fixes #4: Added secret verification

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -3,5 +3,5 @@ import os
 def raise_error(message):
     raise Exception(message)
 
-#NACHET_SCHEMA = os.getenv("NACHET_SCHEMA") or raise_error("NACHET_SCHEMA is not set")
-#NACHET_DB_URL = os.getenv("NACHET_DB_URL") or raise_error("NACHET_DB_URL is not set")
+NACHET_SCHEMA = os.getenv("NACHET_SCHEMA") or raise_error("NACHET_SCHEMA is not set")
+NACHET_DB_URL = os.getenv("NACHET_DB_URL") or raise_error("NACHET_DB_URL is not set")
