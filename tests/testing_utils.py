b31b8cddfca868c87fbdede1b4c37e86104af965
Francois-Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-03-27 19:07:39 +0000

    Issue #4: Added custom errors

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -2,6 +2,13 @@ import os
 
 def raise_error(message):
     raise Exception(message)
+class EnvironmentVariableError(Exception):
+    pass
 
-NACHET_SCHEMA = os.getenv("NACHET_SCHEMA") or raise_error("NACHET_SCHEMA is not set")
-NACHET_DB_URL = os.getenv("NACHET_DB_URL") or raise_error("NACHET_DB_URL is not set")
+
+NACHET_SCHEMA = os.getenv("NACHET_SCHEMA")
+if not NACHET_SCHEMA:
+    raise EnvironmentVariableError("NACHET_SCHEMA is not set")
+NACHET_DB_URL = os.getenv("NACHET_DB_URL") 
+if not NACHET_DB_URL:
+    raise EnvironmentVariableError("NACHET_DB_URL is not set")
